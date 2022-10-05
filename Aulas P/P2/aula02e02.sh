46052340cae055c5bf323bb12347fd8d63848c45
jnluis <jnluis@ua.pt>
2022-10-05 16:25:22 +0100

    P2

---
+++ Aulas P/P2/aula02e02.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Atenção aos espaços
+echo Este e um teste
+echo "Este      e     um     teste"
+#no segundo caso, ele tira os espaços
---
+++ Aulas P/P2/px.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# (( $# <1)) && echo -e "Indique ficheiro(s)\n" ||
+chmod u+x $*
